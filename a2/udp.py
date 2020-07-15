f4abbdaa29228f6862fa06eaf5a3d22d2dc7154c
Ze Ran Lu <zeranlu@hotmail.com>
2020-07-15 16:58:43 -0400

    update comments

--- a2/udp.py
+++ a2/udp.py
@@ -2,22 +2,26 @@ from packet import packet
 from socket import *
 
 
+# Create a socket for sending UDP packets
 def sock_send():
     sock = socket(AF_INET, SOCK_DGRAM)
     return sock
 
 
+# Create a socket for receiving UDP packets
 def sock_recv(port):
     sock = socket(AF_INET, SOCK_DGRAM)
     sock.bind(('', port))
     return sock
 
 
+# Send packet using a socket
 def send_packet(sock, addr, port, pack):
     udp_data = pack.get_udp_data()
     sock.sendto(udp_data, (addr, port))
 
 
+# Receive packet from socket
 def recv_packet(sock):
     udp_data, src_addr = sock.recvfrom(1024)
     pack = packet.parse_udp_data(udp_data)
